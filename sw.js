ad5293f5a69ee3ce2817f1aeeada1f0cef67b408
gorkatrece <gorka.treceno@gmail.com>
2025-12-15 13:16:38 -0500

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = "emom-pwa-v1";
+const CACHE_NAME = "emom-pwa-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
 
   "./russian_swing.mp4",
   "./alt_bent_rows.mp4",
@@ -17,16 +19,14 @@ const FILES_TO_CACHE = [
   "./rest.mp4"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
+self.addEventListener("install", e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
